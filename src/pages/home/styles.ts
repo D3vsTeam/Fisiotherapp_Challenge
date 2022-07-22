fe83a9309d1d4de2ca3a071ec9bca5ec257e66ce
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-07-22 17:57:49 -0300

    create: style file

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  height: 100vh;
+
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`
+
+export const ButtonStyled = styled.Button`
+  margin-top: 1rem;
+  padding: .5rem 1rem;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  margin-bottom: 16px;
+`;
+
+export const Subtile = styled.Text`
+  font-size: 16px;
+  margin-bottom: 32px;
+`
